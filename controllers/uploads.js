26c2a773a7ed7025649ddcabe00b6c8cbb990f03
nico1612 <nicogonzalezraggi123@hotmail.com>
2023-05-23 09:24:23 -0300

    subir archivos helper

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,35 +1,16 @@
 import { response } from "express";
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-uuidv4()
+import { subirArchivos } from "../helpers/subir-archivo.js";
 
-export const cargarArchivos=(req,res=response)=>{
+export const cargarArchivos=async(req,res=response)=>{
 
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-      res.status(400).json({msg:'No hay archivos que subir'});
-      return;
-    }
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(400).json({msg:'No hay archivos que subir'});
+    return;
+  }
 
-    const {archivo}=req.files
-    const nombreCortado=archivo.name.split('.')
-    const extension = nombreCortado[nombreCortado.length-1]
+  const pathCompleto= await subirArchivos(req.files)
 
-    const extensionesValidas=['png','jpg','jpeg','gif']
-    if(!extensionesValidas.includes(extension)){
-        return res.status(400).json({
-            msg:`la extension ${extension} no es valida, ${extensionesValidas}`
-        })
-    }
-
-    const nombreTemporal=uuidv4()+'.'+extension
-    const uploadPath = path (__dirname + '../uploads/' + nombreTemporal);
-  
-    archivo.mv(uploadPath, (err)=>{
-      if (err) {
-        return res.status(500).json(err);
-      }
-  
-      res.json({msg :'File uploaded to ' + uploadPath});
-    });
-
+  req.json({
+    path:pathCompleto
+  })
 }
